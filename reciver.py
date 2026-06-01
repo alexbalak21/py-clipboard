eb69db0981763e9968033c1824f333538e480a58
alexbalak21 <alex.balak@outlook.com>
2026-06-01 17:27:27 +0200

    created script for getting ip of sender and reviver

--- reciver.py
+++ reciver.py
@@ -1,7 +1,7 @@
 import socket
 import pyperclip
 
-HOST = "0.0.0.0"
+HOST = "192.168.1.44"
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 import socket
 import pyperclip
 
-HOST = "192.168.1.50"   # IP of the HOST PC
+HOST = "192.168.1.44"   # IP of the HOST PC
 PORT = 5000
 
 def send_clipboard():
